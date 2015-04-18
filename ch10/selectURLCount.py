1d6f650a574b51187719580cce923e582d0ac2cd
Megan Squire <megansquire@gmail.com>
2015-04-18 17:39:09 -0400

    Create selectURLCount.py
    Python script to pull out URLs from tweet collection and write to tsv

---
+++ ch10/selectURLCount.py
@@ -0,0 +1,22 @@
+# selectURLCount.py
+import csv
+import MySQLdb
+
+# Open database connection
+db = MySQLdb.connect(host="localhost",\
+    user="username", \
+    passwd="password", \
+    db="ferguson", \
+    use_unicode=True, \
+    charset="utf8")
+cursor = db.cursor()
+
+cursor.execute('SELECT left(tdisplay, LOCATE(\'/\', 
+    tdisplay)-1) as url, COUNT(tid) as num 
+    FROM ferguson_tweets_urls 
+    GROUP BY 1 ORDER BY 2 DESC LIMIT 15')
+
+with open('fergusonURLcounts.tsv', 'wb') as fout:
+    writer = csv.writer(fout)
+    writer.writerow([ i[0] for i in cursor.description ])
+    writer.writerows(cursor.fetchall())
